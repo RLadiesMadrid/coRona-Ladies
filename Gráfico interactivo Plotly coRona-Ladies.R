44d8ed0e8736e5bb242988cf5125d2ac09266af3
Juanjo Beunza <juanjo@juanjobeunza.com>
2020-04-02 05:56:27 +0200

    #6 Simplificado código

--- Gráfico interactivo Plotly coRona-Ladies.R
+++ Gráfico interactivo Plotly coRona-Ladies.R
@@ -6,9 +6,7 @@
 
 
 # Activar librerías
-library(tidyverse)
 library(plotly)
-library(ggrepel)
 
 ######################            GRÁFICO INTERACTIVO           #####################
 
@@ -18,7 +16,6 @@ g1 <- ggplot(covid_casos, aes(x = fecha, y = total, color = CCAA, label = CCAA))
   geom_line(aes(group = CCAA)) +
   theme(axis.text.x = element_text(angle = 45))
 ggplotly(g1)
-
 
 # Fallecidos
 g2 <- ggplot(covid_fallecidos, aes(x = fecha, y = total, color = CCAA, label = CCAA)) +
@@ -49,5 +46,3 @@ g5 <- ggplot(covid_hospitalizados, aes(x = fecha, y = total, color = CCAA, label
   geom_line(aes(group = CCAA)) +
   theme(axis.text.x = element_text(angle = 45))
 ggplotly(g5)
-
-#
