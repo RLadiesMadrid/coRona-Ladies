b4487d3bb8bdd02246e320c03328923212f671be
Juanjo Beunza <juanjo@juanjobeunza.com>
2020-04-02 19:13:25 +0200

    Primer draft del Dashboard con CCAA (checkboxGroupInput). Evitado valor nulo (req). Usado libreria shinydashboard pq no piloto mucho el diseño web desde Shiny.

--- Dashboard/app.R
+++ Dashboard/app.R
@@ -1,22 +1,37 @@
 library(shiny)
-library(plotly)
+library(tidyverse)
+library(shinydashboard)
 
-ui <- fluidPage(
-   titlePanel("Dashboard coRona-Ladies"),
-   checkboxGroupInput("CCAA", "Comunidades Autónomas:",
-                      levels(CCAA_levels), "España"),
-   plotOutput(outputId = "plot")
-        )
+ui <- dashboardPage(
+   dashboardHeader(title = "coRona-Ladies"),
+
+   dashboardSidebar(
+   checkboxGroupInput(inputId = "CCAA",  "Comunidades Autónomas:",
+                      levels(CCAA_levels), "España")
+   ),
    
+
+   dashboardBody(
+   plotOutput(outputId = "plot")
+   )
+   )
+
 server <- function(input, output) {
-  output$plot <- renderPlot({
-    ggplotly(ggplot(covid_casos, aes(x = fecha, y = total, color = CCAA, label = CCAA)) +
+   
+   output$list <- renderText({
+      input$CCAA
+   })
+   
+   output$plot <- renderPlot({
+     req(input$CCAA)       # Evita valores nulos
+     covid_casos_g <- covid_casos %>% filter(CCAA == input$CCAA)
+     ggplot(covid_casos_g, aes(x = fecha, y = total, color = CCAA, label = CCAA)) +
                geom_point() +
                geom_line(aes(group = CCAA)) +
-               theme(axis.text.x = element_text(angle = 45)))
-  })
-  
-   }
+               theme(axis.text.x = element_text(angle = 45))
+   })
+   
+}
 
 shinyApp(ui = ui, server = server)
 
